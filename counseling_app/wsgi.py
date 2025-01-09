d20928d5a42af2ccbf5d1ce8a8653d33e2cb698e
Stephen Castaneda <stephencastaneda@MacBookPro.lan>
2025-01-09 01:24:14 -0800

    add

--- counseling_app/wsgi.py
+++ counseling_app/wsgi.py
@@ -1,16 +1,17 @@
-"""
-WSGI config for counseling_app project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
-
 import os
-
+import logging
 from django.core.wsgi import get_wsgi_application
+
+logger = logging.getLogger('wsgi')
+logger.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+logger.addHandler(ch)
+
+logger.debug("Starting WSGI application...")
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'counseling_app.settings')
 
 application = get_wsgi_application()
+
+logger.debug("WSGI application loaded successfully.")
